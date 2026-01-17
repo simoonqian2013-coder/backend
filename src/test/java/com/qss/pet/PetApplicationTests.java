a7c9058d5af850638792f483d0eba36466b3bcf6
Quinnss <2183907473@qq.com>
2026-01-17 11:16:43 +0800

    修改sql密码

--- src/test/java/com/qss/pet/PetApplicationTests.java
+++ src/test/java/com/qss/pet/PetApplicationTests.java
@@ -2,12 +2,14 @@ package com.qss.pet;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootTest
 class PetApplicationTests {
 
 	@Test
 	void contextLoads() {
+		System.out.println(new BCryptPasswordEncoder().encode("admin123"));
 	}
 
 }
